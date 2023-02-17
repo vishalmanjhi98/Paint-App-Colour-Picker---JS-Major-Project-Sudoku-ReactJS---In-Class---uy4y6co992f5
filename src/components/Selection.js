66bc1a5264871fc3d8cb1675e5ad83e84740542b
Vishal Manjhi <vmanjhi98@gmail.com>
2023-02-17 23:06:28 +0530

    first commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React, { useState } from "react";
 
+export default function Selection(props) {
+  let [style, setStyle] = useState({ background: "" });
+
+  let applyColor = props.applyColor;
+
+  return (
+    <div className="fix-box" onClick={() => applyColor(setStyle)} style={style}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+}
